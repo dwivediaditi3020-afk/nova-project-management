5143c88fd3edd7e767d42364912aced9f5c16c8b
Aditi Dwivedi <dwivediaditi3020@gmail.com>
2026-09-08 15:28:27 +0530

    Prepare frontend for Vercel deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://nova-project-management-vcyt.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
